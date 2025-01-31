6fd388da2401982b26e680305d8c19193a947e1f
Nikita <nike5995st@gmail.com>
2025-02-01 01:06:47 +0200

    changed tokens to variables

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # тут будут находится все чувствительные и не публичные данные, по крайней мере пока ты не разберёшься, что такое .env
-TOKEN = '8121836017:AAGdvwVv-IQedPJgQJhEh4ozDtvXctuCRe8' #bot token from BotFather
-DEEPL_API_KEY = '062b6dbc-4448-40ef-83de-7371a1170ad0:fx'
+TOKEN = 'TELEGRAM_BOT_TOKEN' #bot token from BotFather
+DEEPL_API_KEY = 'DEEPL_TOKEN'
