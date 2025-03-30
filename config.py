cdc3e0576c209234a0d1c659bd236fd452b7cae1
Nikita <nike5995st@gmail.com>
2025-03-30 18:28:50 +0300

    add file env, changed tokens

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 # тут будут находится все чувствительные и не публичные данные, по крайней мере пока ты не разберёшься, что такое .env
-TOKEN = 'TELEGRAM_BOT_TOKEN' #bot token from BotFather
-DEEPL_API_KEY = 'DEEPL_TOKEN'
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # Загружает переменные окружения из .env
+
+TOKEN = os.getenv('TELEGRAM_TOKEN') #bot token from BotFather
+DEEPL_API_KEY = os.getenv('DEEPL_API_KEY')
